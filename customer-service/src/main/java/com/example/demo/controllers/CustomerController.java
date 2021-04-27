31285927db0e077915bd4f18b6375d4a6e572dcc
srivatsan <vatsank@gmail.com>
2021-04-27 17:28:29 +0530

    Update with Form Data Handling

--- customer-service/src/main/java/com/example/demo/controllers/CustomerController.java
+++ customer-service/src/main/java/com/example/demo/controllers/CustomerController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,4 +42,13 @@ public class CustomerController {
 		return new ResponseEntity<Customer>(added,HttpStatus.CREATED);
 	}
 	
+	
+	@PostMapping(path = "/form/customers", produces = "application/json", 
+			 consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ResponseEntity<Customer> addCustomerUsingForm(@ModelAttribute Customer entity) {
+		
+		Customer added = this.service.addCustomer(entity);
+		
+		return new ResponseEntity<Customer>(added,HttpStatus.CREATED);
+	}
 }
