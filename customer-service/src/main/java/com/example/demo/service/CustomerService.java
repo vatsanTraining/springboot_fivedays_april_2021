0b0def7547a06412340cd65d408076c951e96009
srivatsan <vatsank@gmail.com>
2021-04-28 10:27:05 +0530

    Update

--- customer-service/src/main/java/com/example/demo/service/CustomerService.java
+++ customer-service/src/main/java/com/example/demo/service/CustomerService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.*;
 
+import javax.persistence.Entity;
+
 import com.example.demo.model.Customer;
 import com.example.demo.repos.CustomerRepository;
 
@@ -26,10 +28,41 @@ public class CustomerService {
 		return this.repo.findAll();
 	}
 	
+	public Optional<Customer> getCustomerById(int id) {
+		
+		 return this.repo.findById(id);
+	}
 	
 	public Customer addCustomer(Customer entity) {
 		
 		return this.repo.save(entity);
 		
 	}
+	
+	public Customer update(Customer customer) {
+		
+		return this.repo.save(customer);
+	}
+	
+	public Optional<Customer> removeCustomer(Customer customer){
+		
+		Optional<Customer> response = Optional.empty();
+		
+		if(this.repo.existsById(customer.getId())) {
+			
+			   this.repo.delete(customer);
+			   
+			   response = Optional.of(customer);
+		}
+		
+		return response;
+	}
+	
+	public List<Customer> findByName(String srchName){
+		
+		 return this.repo.findByName(srchName);
+		
+	}
+	
+	
 }
