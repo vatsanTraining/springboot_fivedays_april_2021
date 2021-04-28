f30c5945790df236f82711044efdf6ddbd6db1e1
srivatsan <vatsank@gmail.com>
2021-04-28 10:37:48 +0530

    Updated the CRUD

--- customer-service/src/main/java/com/example/demo/controllers/CustomerController.java
+++ customer-service/src/main/java/com/example/demo/controllers/CustomerController.java
@@ -3,9 +3,12 @@ package com.example.demo.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,5 +53,34 @@ public class CustomerController {
 		Customer added = this.service.addCustomer(entity);
 		
 		return new ResponseEntity<Customer>(added,HttpStatus.CREATED);
+	}
+	
+	
+	@GetMapping(path = "/customers/{id}")
+	public Customer getCustomerBYid(@PathVariable("id") int id)
+	{
+		
+		 return this.service.getCustomerById(id).
+				  orElseThrow(()-> new RuntimeException("Customer Id Not Found"));
+	}
+	
+	@PutMapping(path = "/customers")
+	public Customer updateDateCustomer(@RequestBody Customer customer) {
+		
+		return this.service.update(customer);
+	}
+	
+	
+	@DeleteMapping(path = "/customers")
+	public Customer removeCustomer(@RequestBody Customer customer) {
+		
+		return this.service.removeCustomer(customer).orElseThrow(()->new RuntimeException("Customer Not Found -Not Delete"));
+		
+	}
+	
+	@GetMapping(path = "/srch/customers/{name}")
+	public List<Customer> findByName(@PathVariable("name") String name){
+		
+		return this.service.findByName(name);
 	}
 }
