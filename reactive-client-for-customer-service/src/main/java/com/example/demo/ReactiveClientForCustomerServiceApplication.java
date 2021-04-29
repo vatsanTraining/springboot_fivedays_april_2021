20b87089ca3e4314c83c29a26e1f4327371023b0
srivatsan <vatsank@gmail.com>
2021-04-29 10:48:31 +0530

    Update

--- reactive-client-for-customer-service/src/main/java/com/example/demo/ReactiveClientForCustomerServiceApplication.java
+++ reactive-client-for-customer-service/src/main/java/com/example/demo/ReactiveClientForCustomerServiceApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ReactiveClientForCustomerServiceApplication {
@@ -10,4 +13,16 @@ public class ReactiveClientForCustomerServiceApplication {
 		SpringApplication.run(ReactiveClientForCustomerServiceApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder builder(){
+		
+		return WebClient.builder();
+	}
+	
+	@Bean
+	public WebClient client(WebClient.Builder builderRef) {
+		
+		return builderRef.build();
+	}
 }
