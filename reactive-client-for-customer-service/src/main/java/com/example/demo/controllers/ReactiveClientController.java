b6df42f1716a4ed23457446b2be572d25cd60cf2
srivatsan <vatsank@gmail.com>
2021-04-29 12:20:34 +0530

    update

--- reactive-client-for-customer-service/src/main/java/com/example/demo/controllers/ReactiveClientController.java
+++ reactive-client-for-customer-service/src/main/java/com/example/demo/controllers/ReactiveClientController.java
@@ -2,10 +2,12 @@ package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class ReactiveClientController {
@@ -14,14 +16,14 @@ public class ReactiveClientController {
 	@Autowired
 	private WebClient client;
 	
-	public static final String BASEURL ="lb://CUSTOMER-SERVICE/api/v1/customers/";
-	@GetMapping(path = "/client/customers")
-	public Flux<String> getAllCustomers(){
+	public static final String BASEURL ="lb://CUSTOMER-SERVICE/api/v1/customers/{id}";
+	@GetMapping(path = "/client/customers/{srchId}")
+	public Mono<String> getAllCustomers(@PathVariable("srchId") int srchId){
 		
 		return client
 				.get()
-				  .uri(BASEURL)
+				  .uri(BASEURL,srchId)
 				   .retrieve()
-				     .bodyToFlux(String.class);
+				     .bodyToMono(String.class);
 	}
 }
