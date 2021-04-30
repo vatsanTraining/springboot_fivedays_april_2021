6eebbd4f1f4738c1049c28155751a181e7ebed8c
srivatsan <vatsank@gmail.com>
2021-04-30 09:49:12 +0530

    Update

--- reactive-client-access-secured-service/src/main/java/com/example/demo/controllers/ClientController.java
+++ reactive-client-access-secured-service/src/main/java/com/example/demo/controllers/ClientController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ClientController {
@@ -10,4 +13,14 @@ public class ClientController {
 	
 	@Autowired
 	WebClient client;
+	
+	
+	@GetMapping(path = "/client")
+	public Flux<String> getAll(){
+		
+		 return client.get().uri("http://localhost:8080/orders/pending")
+		 .headers(headers -> headers.setBasicAuth("chennai","chennai"))
+		 .retrieve().bodyToFlux(String.class);
+		
+	}
 }
