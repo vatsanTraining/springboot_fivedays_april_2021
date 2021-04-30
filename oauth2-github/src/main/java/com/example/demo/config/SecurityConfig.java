d345cb65c356b270973cc0a485867dd80a3047fe
srivatsan <vatsank@gmail.com>
2021-04-30 12:37:17 +0530

    Update

--- oauth2-github/src/main/java/com/example/demo/config/SecurityConfig.java
+++ oauth2-github/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,5 +1,23 @@
 package com.example.demo.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/laptop/**")
+		            .authenticated().and().oauth2Login();
+	
+	}
+	
+	
+	
+	
+	
 
 }
--- oauth2-github/src/main/java/com/example/demo/controllers/CatalogController.java
+++ oauth2-github/src/main/java/com/example/demo/controllers/CatalogController.java
@@ -1,5 +1,23 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CatalogController {
 
+	
+	
+	@GetMapping(path = "/")
+	public String home() {
+		
+		return "Home Page";
+	}
+
+	@GetMapping(path = "/laptop")
+    public String electoronics() {
+		
+		return "Popular Laptops";
+	}
+	
 }
